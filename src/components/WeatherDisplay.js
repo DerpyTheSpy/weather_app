336ded4bdc1989f9888e52306b92a35b8a10e9a3
DerpyTheSpy <erickim334@gmail.com>
2024-05-02 16:55:41 -0400

    some changes to package-lock.json

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -12,43 +12,43 @@ const WeatherDisplay = ({ data, location }) => {
     }
   }
   return (
-    <div className="weather-output">
-  <h2>{data.name}</h2>
-  <div className="weather-details">
-    <div className="detail">
-      <label>Current Temp:</label>
-      <span>{data.main.temp}°C</span>
-    </div>
-    <div className="detail">
-      <label>Sky Conditions:</label>
-      <span>{data.weather[0].description}</span>
-    </div>
-    <div className="detail">
-      <label>Humidity:</label>
-      <span>{data.main.humidity}%</span>
-    </div>
-    <div className="detail">
-      <label>Feels Like:</label>
-      <span>{data.main.feels_like}°C</span>
-    </div>
-    <div className="detail">
-      <label>Wind:</label>
-      <span>{data.wind.speed} m/s</span>
-    </div>
-    <div className="detail">
-      <label>Sunrise:</label>
-      <span>{new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</span>
-    </div>
-    <div className="detail">
-      <label>Sunset:</label>
-      <span>{new Date(data.sys.sunset * 1000).toLocaleTimeString()}</span>
-    </div>
-    <div className="detail">
-      <label>Visibility:</label>
-      <span>{data.visibility / 1000} km</span>
-    </div>
+  <div className="weather-output">
+    <h2>{data.name}</h2>
+      <div className="weather-details">
+        <div className="detail">
+          <label>Current Temp:</label>
+          <span>{data.main.temp}°C</span>
+        </div>
+        <div className="detail">
+          <label>Sky Conditions:</label>
+          <span>{data.weather[0].description}</span>
+        </div>
+        <div className="detail">
+          <label>Humidity:</label>
+          <span>{data.main.humidity}%</span>
+        </div>
+        <div className="detail">
+          <label>Feels Like:</label>
+          <span>{data.main.feels_like}°C</span>
+        </div>
+        <div className="detail">
+          <label>Wind:</label>
+          <span>{data.wind.speed} m/s</span>
+        </div>
+        <div className="detail">
+          <label>Sunrise:</label>
+          <span>{new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</span>
+        </div>
+        <div className="detail">
+          <label>Sunset:</label>
+          <span>{new Date(data.sys.sunset * 1000).toLocaleTimeString()}</span>
+        </div>
+        <div className="detail">
+          <label>Visibility:</label>
+          <span>{data.visibility / 1000} km</span>
+        </div>
+      </div>
   </div>
-</div>
   );
 };
 
