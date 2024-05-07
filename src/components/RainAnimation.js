4317e227694676be9b0791fbf1f60d75bc043512
DerpyTheSpy <erickim334@gmail.com>
2024-05-07 11:51:55 -0400

    added background animation but it doesnt appear to be working properly. will debug later.

---
+++ src/components/RainAnimation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './RainAnimation.css';
+
+const RainAnimation = () => {
+  return (
+    <div className="rain-container">
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+      <div className="rain"></div>
+    </div>
+  );
+};
+
+export default RainAnimation;
